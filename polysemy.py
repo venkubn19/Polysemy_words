c2fa426fc02c4ce65100a09826fc7f664061f66a
venkateshbn <venkubn19@gmail.com>
2019-03-31 16:54:40 +0530

    Add files via upload

---
+++ polysemy.py
@@ -0,0 +1,13 @@
+def poly():
+ polysemy1=['record','bank','book','park','flat','right','left','match','face','board','sharp']
+ return polysemy1
+
+def clues():
+ dic={ 
+      "bank":["fish","fishing","river","trout"]  ,
+      "park":["vehicle","car","cycle","bike"],
+      "book":["flight","ticket","plane","show","movie"],
+      "match":["cricket","football"]
+     }
+ return dic
+#record bank book park flat right match face board sharp
